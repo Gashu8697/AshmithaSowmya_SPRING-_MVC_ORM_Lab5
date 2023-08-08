76d21c8822453ef722a967cedc82d8cc32238802
Ashmitha Sowmya <76016259+Gashu8697@users.noreply.github.com>
2023-08-08 18:15:44 +0530

    Add files via upload

---
+++ ASLab5/ASLab5/src/main/java/com/greatLearning/Student/ASLab5/AsLab5Application.java
@@ -0,0 +1,15 @@
+package com.greatLearning.Student.ASLab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AsLab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsLab5Application.class, args);
+	}
+
+}
---
+++ ASLab5/ASLab5/src/test/java/com/greatLearning/Student/ASLab5/AsLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatLearning.Student.ASLab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
